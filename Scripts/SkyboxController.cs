1939a4b5418ec58f2ec043b0e83d1aaaa241a1fe
iulian24-cyber <iulian2414@gmail.com>
2025-11-10 19:53:34 +0200

    finished skybox day/night time cycle + stars

--- Scripts/SkyboxController.cs
+++ Scripts/SkyboxController.cs
@@ -7,10 +7,12 @@ public partial class SkyboxController : WorldEnvironment
 	public override void _Process(double delta)
 	{
 		Vector3 sun_dir = GetNode<Node3D>("../Sun").GlobalTransform.Basis.Z;
+		var moon_basis = GetNode<Node3D>("../Moon").GlobalTransform.Basis;
 		Vector3 moon_dir = GetNode<Node3D>("../Moon").GlobalTransform.Basis.Z;
 
 		var skyMat = Environment?.Sky?.SkyMaterial as ShaderMaterial;
 		skyMat.SetShaderParameter("sun_dir", sun_dir);
 		skyMat.SetShaderParameter("moon_dir", moon_dir);
+		skyMat.SetShaderParameter("moon_to_world_to_object", moon_basis.Inverse());
 	}
 }
